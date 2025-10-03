6aa6256c225eaef9f4c5604541e22069ac86b696
Toukara <benjamincms62@gmail.com>
2025-10-03 05:44:43 +0200

    rss feed updated

--- src/components/RSS/DevTo.js
+++ src/components/RSS/DevTo.js
@@ -41,22 +41,38 @@ function RSS_DevTo() {
     <div>
       <ul>
         {articles.slice(0, 8).map((article, idx) => (
-          <li key={idx} style={{ marginBottom: 20 }}>
-            <a href={article.link} target="_blank" rel="noopener noreferrer">
-              <h4 style={{ marginBottom: 5 }}>{article.title}</h4>
-            </a>
-            <p>
-              by {article.creator} on{" "}
-              {new Date(article.pubDate).toLocaleDateString()}
-            </p>
-            <p dangerouslySetInnerHTML={{ __html: article.description }}></p>
-            <p>Categories: {article.categorys}</p>
-            <hr />
-          </li>
+          // console.log(idx ,article ),
+          <article className="message rss-widget">
+            <div className="message-body" style={{padding: '50px', paddingBottom: "30px" , maxWidth: '650px'}}>
+              <li key={idx} style={{ marginBottom: 50 }}>
+                <a
+                  href={article.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <h4 style={{ marginBottom: 5, fontSize: 18, fontWeight: "bold" }}>{article.title}</h4>
+                </a>
+
+                <p>
+                  by <strong> {article.creator} </strong> on {""}
+                  <em>{new Date(article.pubDate).toLocaleDateString()}</em>
+                </p>
+                <br />
+                <p
+                  dangerouslySetInnerHTML={{ __html: article.description }}
+                ></p>
+                <p>Categories: {article.categorys}</p>
+                <hr />
+              </li>
+            </div>
+                <div className="separator"></div>
+          </article>
         ))}
       </ul>
     </div>
   );
 }
+
+
 
 export default RSS_DevTo;
